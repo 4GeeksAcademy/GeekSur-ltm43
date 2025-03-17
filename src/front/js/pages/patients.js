432afaf57e3c5fe6d974abaa5aab7922d3be4cd8
Alejandro Arraga <alejandroarraga99@gmail.com>
2025-03-17 22:29:32 +0000

    added button back home

--- src/front/js/pages/patients.js
+++ src/front/js/pages/patients.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Patients = () => {
@@ -169,6 +170,10 @@ export const Patients = () => {
           </li>
         ))}
       </ul>
+      <br />
+                <Link to="/">
+                  <button className="btn btn-primary">Back home</button>
+                </Link>
     </div>
   );
 };
